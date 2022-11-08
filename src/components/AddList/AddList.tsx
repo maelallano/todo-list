c982cf0b52ebf6a136526e6ff10958dcf03eaa2d
Maël ALLANO <maelallano1@gmail.com>
2022-11-08 11:21:08 +0100

    AddList done

--- src/components/AddList/AddList.tsx
+++ src/components/AddList/AddList.tsx
@@ -1,68 +1,74 @@
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import styles from "./AddList.module.scss";
 
 import { ListsType } from "helpers/types";
 import { addListLS } from "helpers/localStorage";
 
+type FormValues = {
+  title: string;
+};
+
 type Props = { setListsData: React.Dispatch<React.SetStateAction<ListsType>> };
 
 const AddList: React.FC<Props> = ({ setListsData }) => {
-  function handleAddList() {
+  const [isAdding, setIsAdding] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    // formState: { errors },
+    setFocus,
+  } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    setIsAdding(false);
+    reset();
+
     const valueToAdd = {
-      id: 1,
-      title: "open",
+      id: 0,
+      title: data.title,
+      // icon: "⭕️",
       color: "#EB5A46",
     };
 
     setListsData(addListLS(valueToAdd));
+  };
+
+  function handleAddList() {
+    setIsAdding(true);
+    setFocus("title");
+  }
+
+  function handleCancel() {
+    setIsAdding(false);
+    reset();
   }
 
   return (
-    <button className={styles.button} onClick={handleAddList}>
-      Add a category
-    </button>
+    <>
+      {isAdding ? (
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("title", { required: true })} />
+            {/* {errors.title && <span>This field is required</span>} */}
+
+            <input type="submit" value="Add a card" />
+          </form>
+          <div>
+            <button onClick={handleCancel} className={styles.button}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      ) : (
+        <button onClick={handleAddList} className={styles.button}>
+          Add a category
+        </button>
+      )}
+    </>
   );
-  // const [isAdding, setIsAdding] = useState(false);
-
-  // function handleAddTodo() {
-  //   setIsAdding(false);
-
-  //   const valueToAdd = {
-  //     id: 1,
-  //     // icon: "⭕️",
-  //     listId: 1,
-  //     title: "TEST",
-  //     description: "Fill out human interest distribution form",
-  //   };
-
-  //   setTodosData(addTodoLS(valueToAdd));
-  // }
-
-  // return (
-  //   <>
-  //     {isAdding ? (
-  //       <div>
-  //         <input />
-  //         <div>
-  //           <button onClick={handleAddTodo} className={styles.button}>
-  //             Add a card
-  //           </button>
-  //           <button
-  //             onClick={() => setIsAdding(false)}
-  //             className={styles.button}
-  //           >
-  //             Cancel
-  //           </button>
-  //         </div>
-  //       </div>
-  //     ) : (
-  //       <button onClick={() => setIsAdding(true)} className={styles.button}>
-  //         Add a card
-  //       </button>
-  //     )}
-  //   </>
-  // );
 };
 
 export default AddList;
